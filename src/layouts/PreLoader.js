5549a366098d34509800e3fdf6cc93ad07ca96fa
Moch Azmi Iskandar <m.azmiiskndr@gmail.com>
2023-08-15 14:24:14 +0700

    🔥 refactor(PreLoader.js): update logo image source to use the correct file name and path 🔥 refactor(logo-azmi.png): update logo image file to a new version with improved design

--- src/layouts/PreLoader.js
+++ src/layouts/PreLoader.js
@@ -8,7 +8,7 @@ const PreLoader = () => {
           <div className="vertical-center">
             <div className="spinner-logo">
               <img
-                src="assets/images/logo-azmi-3.png"
+                src="assets/images/logo-azmi.png"
                 alt="Moch Azmi Iskandar - Personal Portfolio"
               />
               <div className="spinner-dot" />
