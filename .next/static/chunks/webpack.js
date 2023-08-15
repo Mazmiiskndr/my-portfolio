6e848693e5b85da5935a2241ee5cf6fafdaea892
Moch Azmi Iskandar <m.azmiiskndr@gmail.com>
2023-08-15 09:10:50 +0700

    :bug: fix commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fffa0ca4d1b9f015"; }
+/******/ 		__webpack_require__.h = function() { return "319db159d189bf41"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
