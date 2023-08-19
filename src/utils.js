1547f890bc5c53be81088ef5e9af58dc198110b5
Moch Azmi Iskandar <m.azmiiskndr@gmail.com>
2023-08-19 22:13:44 +0700

    🚀 feat(utils.js): add truncateText function to truncate text if it exceeds a specified length

--- src/utils.js
+++ src/utils.js
@@ -138,3 +138,11 @@ export const jarallaxAnimation = () => {
     type: "scroll",
   });
 };
+
+export const truncateText = (text, length) => {
+  if (text.length > length) {
+    return text.substring(0, length) + "...";
+  } else {
+    return text;
+  }
+};
