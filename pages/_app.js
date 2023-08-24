b2291d0bcb46527db92e3a32fefeeebfe726da51
Moch Azmi Iskandar <m.azmiiskndr@gmail.com>
2023-08-24 19:18:25 +0700

    🔧 fix(_app.js): update meta tags for SEO optimization and improve page description and author information

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,34 @@ const App = ({ Component, pageProps }) => {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="HandheldFriendly" content="true" />
-        <meta name="author" content="bslthemes" />
+        <meta
+          name="title"
+          content="Moch Azmi Iskandar - Experienced Software Developer and Programmer"
+        />
+        <meta
+          name="description"
+          content="Portfolio of Moch Azmi Iskandar, a skilled developer specialized in HTML, CSS, Javascript, ReactJS, VueJS, NextJS, InertiaJS, PHP, 
+          Codeigniter, Laravel, MySQL, and more. Explore projects, skills, and contact information."
+        />
+        <meta
+          name="keywords"
+          content="Moch Azmi Iskandar, Software Developer, Web Development, HTML, CSS, Javascript, PHP, Codeigniter, Laravel, MySQL"
+        />
+        <meta name="author" content="Moch Azmi Iskandar" />
+        <meta name="robots" content="index, follow" />
+        <meta
+          property="og:title"
+          content="Moch Azmi Iskandar - Experienced Software Developer and Programmer"
+        />
+        <meta
+          property="og:description"
+          content="Explore the portfolio of Moch Azmi Iskandar and discover his extensive experience in web development using modern technologies."
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:site_name"
+          content="Moch Azmi Iskandar Portfolio"
+        />
       </Head>
       {loader && <PreLoader />}
       <Component {...pageProps} />
